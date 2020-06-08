b8c9b71ee7dced5c8d3082d17bf00b93269dad72
Sumit Sijher <sumitsijher@gmail.com>
2020-06-07 19:36:28 -0700

    added animation record

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-var t= 0;
+var record_animation = false;
+var name = "image_"
+var total_frames = 400;
+var frame = 0;
+var loop = 0;
+var total_time = Math.PI;
+var rate = total_time/total_frames;
+
+var s = 0;
+var t = 0;
 var c = 0;
-var rate = 0.25;
+//var rate = 0.25;
 
 var get_mouse_pos = false;
 var get_touch_pos = false;
@@ -12,18 +21,20 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 
-startAnimating(60);
+startAnimating(30);
 
 
 function draw() {
 
-  canvas.width = window.innerWidth ;
-  canvas.height = window.innerHeight;
+  canvas.width = 300; //window.innerWidth ;
+  canvas.height = 300; //window.innerHeight;
 
+  ctx.fillStyle = 'rgba(255,255,255,1)';
+  ctx.fillRect(0,0,canvas.width,canvas.height);
   ctx.fillStyle = 'rgba(0,0,0,.5)';
   ctx.fillRect(0,0,canvas.width,canvas.height);
   
-  let s = .0005*t;
+  //let s = .0005*t;
   ctx.lineWidth = 12;
   for(i=0;i< Math.max(canvas.width, canvas.height); i+=10){
     ctx.strokeStyle = `hsla(${(0.5*i + 1*c)%360},90%,80%,1)`;
@@ -32,8 +43,8 @@ function draw() {
     ctx.stroke();
   }
   
-  t += 3*rate;
-  c += 1;
+  //t += 3*rate;
+  //c += 1;
   
 }
 
@@ -61,7 +72,34 @@ function startAnimating(fps) {
     if (elapsed > fpsInterval) {
         then = now - (elapsed % fpsInterval);
     
-        draw();  
+        draw();
+        frame = (frame+1)%total_frames;
+        time = rate*frame;
+        s = t = -Math.PI*(Math.cos(time) - 1)/2;
+        c = 360*frame/total_frames;
+
+        if(record_animation) {
+
+            if (loop === 1) { 
+            let frame_number = frame.toString().padStart(total_frames.toString().length, '0');
+            let filename = name+frame_number+'.png'
+                
+            dataURL = canvas.toDataURL();
+            var element = document.createElement('a');
+            element.setAttribute('href', dataURL);
+            element.setAttribute('download', filename);
+            element.style.display = 'none';
+            document.body.appendChild(element);
+            element.click();
+            document.body.removeChild(element);
+            }
+
+            if (frame + 1 === total_frames) {
+                loop += 1;
+            }
+
+            if (loop === 2) { stop_animation = true }
+        }
     }
 
     canvas.addEventListener('mousedown', e => {
